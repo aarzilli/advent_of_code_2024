9185c3dc06f29bc9fee1a50cd8a4e05557ffd169
aarzilli <alessandro.arzilli@gmail.com>
2024-11-30 15:18:53 +0100

    initial commit

---
+++ template.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	. "aoc/util"
+	"fmt"
+	"os"
+)
+
+func pf(fmtstr string, any ...interface{}) {
+	fmt.Printf(fmtstr, any...)
+}
+
+func pln(any ...interface{}) {
+	fmt.Println(any...)
+}
+
+func main() {
+	lines := Input(os.Args[1], "\n", true)
+	pf("len %d\n", len(lines))
+}

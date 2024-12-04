c44a293696d647473b70a484d26c524cf37cfa34
aarzilli <alessandro.arzilli@gmail.com>
2024-12-04 06:41:43 +0100

    day 4 cleanup

--- 04.go
+++ 04.go
@@ -2,17 +2,8 @@ package main
 
 import (
 	. "aoc/util"
-	"fmt"
 	"os"
 )
-
-func pf(fmtstr string, any ...interface{}) {
-	fmt.Printf(fmtstr, any...)
-}
-
-func pln(any ...interface{}) {
-	fmt.Println(any...)
-}
 
 var lines []string
 
@@ -23,22 +14,17 @@ func get(i, j int) byte {
 	return lines[i][j]
 }
 
-func check(si, sj, di, dj int) bool {
-	return get(si, sj) == 'X' && get(si+di, sj+dj) == 'M' && get(si+2*di, sj+2*dj) == 'A' && get(si+3*di, sj+3*dj) == 'S'
-}
-
-func check2(si, sj, di, dj int) bool {
-	return get(si, sj) == 'M' && get(si+di, sj+dj) == 'A' && get(si+2*di, sj+2*dj) == 'S'
-}
-
-func p2(i, j int) bool {
-	return (check2(i-1, j-1, +1, +1) || check2(i+1, j+1, -1, -1)) &&
-		(check2(i-1, j+1, +1, -1) || check2(i+1, j-1, -1, +1))
+func checkstr(si, sj, di, dj int, str string) bool {
+	for i := range str {
+		if get(si+i*di, sj+i*dj) != str[i] {
+			return false
+		}
+	}
+	return true
 }
 
 func main() {
 	lines = Input(os.Args[1], "\n", true)
-	pf("len %d\n", len(lines))
 	cnt := 0
 	for i := range lines {
 		for j := range lines[i] {
@@ -48,7 +34,7 @@ func main() {
 						if di == 0 && dj == 0 {
 							continue
 						}
-						if check(i, j, di, dj) {
+						if checkstr(i, j, di, dj, "XMAS") {
 							cnt++
 						}
 					}
@@ -62,7 +48,7 @@ func main() {
 	for i := range lines {
 		for j := range lines {
 			if lines[i][j] == 'A' {
-				if p2(i, j) {
+				if (checkstr(i-1, j-1, +1, +1, "MAS") || checkstr(i+1, j+1, -1, -1, "MAS")) && (checkstr(i-1, j+1, +1, -1, "MAS") || checkstr(i+1, j-1, -1, +1, "MAS")) {
 					cnt2++
 				}
 			}

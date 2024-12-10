2c500982ec9e80d7c2faf5f70325213043e826f1
aarzilli <alessandro.arzilli@gmail.com>
2024-12-10 09:50:18 +0100

    day 10 cleanup

--- 10.go
+++ 10.go
@@ -2,17 +2,8 @@ package main
 
 import (
 	. "aoc/util"
-	"fmt"
 	"os"
 )
-
-func pf(fmtstr string, any ...interface{}) {
-	fmt.Printf(fmtstr, any...)
-}
-
-func pln(any ...interface{}) {
-	fmt.Println(any...)
-}
 
 type pos struct {
 	i, j int
@@ -22,7 +13,6 @@ var M [][]int
 
 func main() {
 	lines := Input(os.Args[1], "\n", true)
-	pf("len %d\n", len(lines))
 	M = make([][]int, len(lines))
 	for i := range lines {
 		M[i] = make([]int, len(lines[i]))
@@ -32,80 +22,33 @@ func main() {
 	}
 
 	part1 := 0
-	for i := range M {
-		for j := range M[i] {
-			if M[i][j] == 0 {
-				k := visit(pos{i, j})
-				pln(k)
-				part1 += k
-			}
-		}
-	}
-	Sol(part1)
-
 	part2 := 0
 	for i := range M {
 		for j := range M[i] {
 			if M[i][j] == 0 {
-				k := visit2(pos{i, j})
-				pln(k)
+				d := make(Set[pos])
+				k := visit2(pos{i, j}, d)
+				part1 += len(d)
 				part2 += k
 			}
 		}
 	}
+	Sol(part1)
 	Sol(part2)
 
 }
 
-func visit(start pos) int {
-	fringe := []pos{start}
-	seen := make(Set[pos])
-	dest := make(Set[pos])
-
-	for len(fringe) > 0 {
-		cur := fringe[0]
-		fringe = fringe[1:]
-		if seen[cur] {
-			continue
-		}
-		seen[cur] = true
-
-		add := func(i, j int) {
-			next := pos{i, j}
-			if next.i < 0 || next.i >= len(M) || next.j < 0 || next.j >= len(M[next.i]) {
-				return
-			}
-			if M[cur.i][cur.j]+1 != M[next.i][next.j] {
-				return
-			}
-			fringe = append(fringe, next)
-		}
-
-		if M[cur.i][cur.j] == 9 {
-			dest[cur] = true
-		}
-
-		add(cur.i+1, cur.j)
-		add(cur.i-1, cur.j)
-		add(cur.i, cur.j+1)
-		add(cur.i, cur.j-1)
-	}
-	return len(dest)
-}
-
-func visit2(cur pos) int {
+func visit2(cur pos, d Set[pos]) int {
 	if M[cur.i][cur.j] == 9 {
+		d[cur] = true
 		return 1
 	}
 	r := 0
 	for _, next := range []pos{pos{cur.i + 1, cur.j}, pos{cur.i - 1, cur.j}, pos{cur.i, cur.j + 1}, pos{cur.i, cur.j - 1}} {
-		if next.i < 0 || next.i >= len(M) || next.j < 0 || next.j >= len(M[next.i]) {
+		if next.i < 0 || next.i >= len(M) || next.j < 0 || next.j >= len(M[next.i]) || M[cur.i][cur.j]+1 != M[next.i][next.j] {
 			continue
 		}
-		if M[cur.i][cur.j]+1 != M[next.i][next.j] {
-			continue
-		}
-		r += visit2(next)
+		r += visit2(next, d)
 	}
 	return r
 }

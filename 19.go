5b71d45692bf684554b46a338bd145f7b7bcfc2c
aarzilli <alessandro.arzilli@gmail.com>
2024-12-20 08:18:37 +0100

    day 20

--- 19.go
+++ 19.go
@@ -3,7 +3,6 @@ package main
 import (
 	. "aoc/util"
 	"os"
-	"regexp"
 	"strings"
 )
 
@@ -11,18 +10,15 @@ func main() {
 	groups := Input(os.Args[1], "\n\n", true)
 	Pf("len %d\n", len(groups))
 	pats := Noempty(Spac(groups[0], ",", -1))
-	Pf("%q\n", pats)
-	rx := regexp.MustCompile("^(" + strings.ReplaceAll(groups[0], ", ", "|") + ")+$")
-	Pln(rx)
 	lines := Noempty(Spac(groups[1], "\n", -1))
 	part1 := 0
 	part2 := 0
+	memo := make(map[string]int)
 	for _, line := range lines {
-		if rx.MatchString(line) {
-			Pln(line, "is possible", rx.FindString(line))
+		n := enum(line, pats, memo)
+		if n != 0 {
+			Pln(line, "is possible, matches", n)
 			part1++
-			n := enum(line, pats, make(map[string]int))
-			Pln("matches", n)
 			part2 += n
 		}
 	}

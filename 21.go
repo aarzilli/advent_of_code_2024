e60beee325d6d8fd4fcd5fb76229c1e880d3741f
aarzilli <alessandro.arzilli@gmail.com>
2024-12-21 11:01:34 +0100

    day part 2

--- 21.go
+++ 21.go
@@ -40,42 +40,145 @@ func main() {
 	Pf("len %d\n", len(lines))
 	//Pf("%q\n", allmoves('N', '7', 'A'))
 	//Pf("%q\n", codesfor('N', "029A"))
+	
 	part1 := 0
 	for _, line := range lines {
 		num := Atoi(line[:len(line)-1])
 		l := multilevellen(line)
+		//l2 := multilevellenold(line)
 		Pln(num, l)
 		part1 += num * l
 	}
 	Sol(part1)
 }
 
+const DIFFICULTY = 26
+//const DIFFICULTY = 3
+
 // part1 example: 126384
 // part1 real: 270084
 
-func multilevellen(s string) int {
+// part2 wrong: 154115708116294 (too low)
+// part2 wrong: 61952932092390 (too low)
+
+func multilevellenold(s string) int {
 	combos := multilevel(s)
 	min := -1
+	mincombo := ""
 	for k := range combos {
 		if min < 0 || len(k) < min {
 			min = len(k)
+			mincombo = k
+		}
+	}
+	Pln("multilevellenold", s, mincombo)
+	return min
+}
+
+func multilevellen(s string) int {
+	cur := byte('A')
+	r := 0
+	for i := range s {
+		ch := s[i]
+		n := multilevellen1('N', DIFFICULTY, cur, ch)
+		//Pln(string(cur), string(ch), n)
+		r += n
+		cur = ch
+	}
+	return r
+}
+
+var multilevellen1memomap = make(map[string]int)
+
+func multilevellen1(padkind byte, n int, start, end byte) int {
+	k := fmt.Sprintf("%d:%c%c%c", n, padkind, start, end)
+	r, ok := multilevellen1memomap[k]
+	if ok {
+		return r
+	}
+	r = multilevellen1real(padkind, n, start, end)
+	//Pln("multilevellen1real", string(padkind), n, "from:", string(start), "to:", string(end), "minsteps:", r)
+	multilevellen1memomap[k] = r
+	return r
+}
+
+func multilevellen1real(padkind byte, n int, start, end byte) int {
+	if n == 0 {
+		return 1
+	}
+	
+	moves := allmoves(padkind, start, end)
+	
+	min := -1
+	for _, move := range moves {
+		cur := byte('A')
+		l := 0
+		for i := range move {
+			ch := move[i]
+			l += multilevellen1('A', n-1, cur, ch)
+			cur = ch
+		}
+		if min < 0 || l < min {
+			min = l
 		}
 	}
 	return min
 }
 
+/*func multilevellen1(start, end byte) int {
+	k := fmt.Sprintf("%c%c", start, end)
+	r, ok := multilevellen1memomap[k]
+	if ok {
+		return r
+	}
+	r = multilevellen1real(start, end)
+	multilevellen1memomap[k] = r
+	return r
+}
+
+func multilevellen1real(start, end byte) int {
+	combos := make(Set[string])
+	combos[string(end)] = true
+	for i := 0; i < 3; i++ {
+		ncombos := make(Set[string])
+		padkind := byte('N')
+		s := start
+		if i > 0 {
+			padkind = 'A'
+			s = 'A'
+		}
+		for combo := range combos {
+			r := codesfor(padkind, s, combo)
+			for _, p := range r {
+				ncombos[p] = true
+			}
+		}
+		combos = ncombos
+	}
+	min := -1
+	mincombo := ""
+	for k := range combos {
+		if min < 0 || len(k) < min {
+			min = len(k)
+			mincombo = k
+		}
+	}
+	Pln("multilevellen1real", string(start), string(end), mincombo, min)
+	return min
+}*/
+
 func multilevel(s string) Set[string] {
 	curcombos := make(Set[string])
 	curcombos[s] = true
 
-	for i := 0; i < 3; i++ {
+	for i := 0; i < DIFFICULTY; i++ {
 		nextcombos := make(Set[string])
 		padkind := byte('N')
 		if i > 0 {
 			padkind = 'A'
 		}
 		for combo := range curcombos {
-			r := codesfor(padkind, combo)
+			r := codesfor(padkind, 'A', combo)
 			for _, p := range r {
 				nextcombos[p] = true
 			}
@@ -87,19 +190,19 @@ func multilevel(s string) Set[string] {
 
 var codesformemomap = make(map[string][]string)
 
-func codesfor(padkind byte, s string) []string {
-	k := fmt.Sprintf("%c%s", padkind, s)
+func codesfor(padkind, start byte, s string) []string {
+	k := fmt.Sprintf("%c%c%s", padkind, start, s)
 	r, ok := codesformemomap[k]
 	if ok {
 		return r
 	}
-	r = codesforreal(padkind, s)
+	r = codesforreal(padkind, start, s)
 	codesformemomap[k] = r
 	return r
 }
 
-func codesforreal(padkind byte, s string) []string {
-	cur := byte('A')
+func codesforreal(padkind, start byte, s string) []string {
+	cur := start
 	r := [][]string{}
 	for i := range s {
 		ch := s[i]
